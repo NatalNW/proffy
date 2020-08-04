270a60c8b09560531e865b2dcc3a152cc94bbcd9
NatalNW <natanaelweslley1@gmail.com>
2020-08-04 20:21:00 -0300

    Routes file

---
+++ web/src/routes.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
+import TeacherForm from './pages/Teacher/Form';
+import TeacherList from './pages/Teacher/List';
+
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Route path='/' component={Landing} />
+            <Route path='/study' component={TeacherList} />
+            <Route path='/teach' component={TeacherForm} />
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
