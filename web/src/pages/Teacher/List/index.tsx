22f5c3bcdcbc9e4e50c86d0bccbcd69599d4b60c
NatalNW <natanaelweslley1@gmail.com>
2020-08-04 23:08:24 -0300

    Styling teachers list

--- web/src/pages/Teacher/List/index.tsx
+++ web/src/pages/Teacher/List/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import PageHeader from '../../../components/PageHeader';
 
+import './style.css'
+
 function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
-            <PageHeader title="These are the available proffies." />
+            <PageHeader title="These are the available proffies.">
+                <form action="" id="search-teachers">
+                    <div className="input-block">
+                        <label htmlFor="subject">Subject</label>
+                        <input type="text" id="subject"/>
+                    </div>
+                    <div className="input-block">
+                        <label htmlFor="week_day">Week Day</label>
+                        <input type="text" id="week_day"/>
+                    </div>
+                    <div className="input-block">
+                        <label htmlFor="time">Time</label>
+                        <input type="text" id="time"/>
+                    </div>
+                </form>
+            </PageHeader>
         </div>
     );
 }
