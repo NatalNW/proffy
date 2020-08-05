e9bda0312cc70dbdfc1c14f1ccd4f2d93f5b748f
NatalNW <natanaelweslley1@gmail.com>
2020-08-05 00:01:18 -0300

    Styling teachers list

--- web/src/pages/Teacher/List/index.tsx
+++ web/src/pages/Teacher/List/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PageHeader from '../../../components/PageHeader';
 
+import whatsappIcon from '../../../assets/images/icons/whatsapp.svg'
+
 import './style.css'
 
 function TeacherList() {
@@ -22,6 +24,35 @@ function TeacherList() {
                     </div>
                 </form>
             </PageHeader>
+
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="https://e4e.org/sites/default/files/2014.11.12.NY_.Patrick_Sprinkle.sq_.jpg" alt="Teacher Name"/>
+                        <div>
+                            <strong>Teacher Name</strong>
+                            <span>Quimica</span>
+                        </div>
+                    </header>
+
+                    <p>
+                        Entusiasta das melhores tecnologias de quimica avançada.
+                        <br/><br/>
+                        Apaixonado por explodir coisas em laboratorio epor mudar a vida das pessoas atraves de experiencias.
+                    </p>
+
+                    <footer>
+                        <p>
+                            Price by time
+                            <strong>R$ 80,00</strong>
+                        </p>
+                        <button type="button">
+                            <img src={whatsappIcon} alt="Whatsapp"/>
+                            Call me
+                        </button>
+                    </footer>
+                </article>
+            </main>
         </div>
     );
 }
