6ef8a1d1955a771b53586007a5b91f7eba232f92
NatalNW <natanaelweslley1@gmail.com>
2020-08-06 16:59:53 -0300

    Add ClassesController

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,65 +1,10 @@
 import express from 'express';
-import db from './database/connection';
-import convertHourToMinutes from './utils/convertHourToMinutes';
+import ClassesController from './controllers/ClassesController';
 
 const routes = express.Router();
+const classesController = new ClassesController();
 
-interface ScheduleItem {
-    week_day: number;
-    from: string;
-    to: string;
-}
-
-routes.post('/classes', async (req, res) => {
-    const {
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subject,
-        cost,
-        schedule
-    } = req.body;
-
-    const trx = await db.transaction();
-
-    try {
-        const usersIds = await trx('users').insert({
-            name,
-            avatar,
-            whatsapp,
-            bio
-        });
-
-        const user_id = usersIds[0];
-
-        const classesIds = await trx('classes').insert({
-            subject,
-            cost,
-            user_id
-        });
-
-        const class_id = classesIds[0];
-
-        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-            return {
-                class_id,
-                week_day: scheduleItem.week_day,
-                from: convertHourToMinutes(scheduleItem.from),
-                to: convertHourToMinutes(scheduleItem.to)
-            }
-        });
-
-        await trx('class_schedule').insert(classSchedule);
-
-        await trx.commit();
-
-        return res.status(201).send();
-    } catch (error) {
-        await trx.rollback();
-        console.log(error);
-        return res.status(400).json({ message: error });
-    }
-});
+routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.create);
 
 export default routes;
