1059e3676a89918e463de453d4be7607bb7d8bef
NatalNW <natanaelweslley1@gmail.com>
2020-08-09 16:37:26 -0300

    Styling submit button

--- web/src/pages/Teacher/List/index.tsx
+++ web/src/pages/Teacher/List/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import PageHeader from '../../../components/PageHeader';
 import TeacherItem from '../../../components/TeacherItem';
 import Input from '../../../components/Input';
@@ -7,13 +7,23 @@ import Select from '../../../components/Select';
 import './style.css'
 
 function TeacherList() {
+    const [subject, setSubject] = useState('');
+    const [week_day, setWeekDay] = useState('');
+    const [time, setTime] = useState('');
+
+    function searchTeachers(e: FormEvent){
+        e.preventDefault();
+    }
+
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="These are the available proffies.">
-                <form action="" id="search-teachers">
+                <form action="" id="search-teachers" onSubmit={searchTeachers}>
                     <Select
                         name="subject"
                         label="Subject"
+                        value={subject}
+                        onChange={e => { setSubject(e.target.value) }}
                         options={[
                             { value: 'Philosophy', label: 'Philosophy' },
                             { value: 'Biology', label: 'Biology' },
@@ -28,6 +38,8 @@ function TeacherList() {
                     <Select
                         name="week_day"
                         label="Week Day"
+                        value={week_day}
+                        onChange={e => { setWeekDay(e.target.value) }}
                         options={[
                             { value: '0', label: 'Monday' },
                             { value: '1', label: 'Tuesday' },
@@ -38,7 +50,17 @@ function TeacherList() {
                             { value: '6', label: 'Sunday' },
                         ]}
                     />
-                    <Input type="time" name="time" label="Time" />
+                    <Input 
+                        type="time" 
+                        name="time" 
+                        label="Time" 
+                        value={time}
+                        onChange={e => { setTime(e.target.value) }}
+                    />
+                    
+                    <button type='submit'>
+                        Search Teachers
+                    </button>
                 </form>
             </PageHeader>
 
